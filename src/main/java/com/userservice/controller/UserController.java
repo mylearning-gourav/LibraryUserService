ca39ff313a72ad74d9369f51219c9ff8682c7a2f
wicked-mind <wickedmind2016@gmail.com>
2018-01-03 01:31:04 +0530

    Added Swagger Functionality.

--- src/main/java/com/userservice/controller/UserController.java
+++ src/main/java/com/userservice/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.userservice.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,12 +17,14 @@ import com.userservice.service.UserService;
 
 @RestController
 @RequestMapping(value="/userservices")
+@Api(value="UserService", description="User Service for Library")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
 	@RequestMapping(value="/getallusers", method=RequestMethod.GET)
+	@ApiOperation(value = "View a list of users", response = ResultBean.class)
 	public ResultBean getAllUser() throws Exception {
 		ResultBean resultBean = new ResultBean();
 		HashMap<String, ArrayList<Object>> userResultMap = new HashMap<String, ArrayList<Object>>();
@@ -29,6 +34,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/registeruser", method=RequestMethod.GET)
+	@ApiOperation(value="Register new user", response=ResultBean.class)
 	public ResultBean registerNewUser(User user) throws Exception {
 		userService.registerNewUser(user);
 		ResultBean resultBean = new ResultBean();
@@ -36,6 +42,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/updateuser", method=RequestMethod.GET)
+	@ApiOperation(value="Update user", response=ResultBean.class)
 	public ResultBean updateUser(User user) throws Exception{
 		userService.updateUser(user);
 		ResultBean resultBean = new ResultBean();
@@ -43,6 +50,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/updateactivestatus", method=RequestMethod.GET)
+	@ApiOperation(value="Update user status", response=ResultBean.class)
 	public ResultBean updateActiveStatus(int userId, boolean active) throws Exception {
 		userService.updateActiveStatus(userId, active);
 		ResultBean resultBean = new ResultBean();
@@ -50,6 +58,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/getactivestatus", method=RequestMethod.GET)
+	@ApiOperation(value="Get user status", response=ResultBean.class)
 	public ResultBean getActiveStatus(int userId) throws Exception {
 		Boolean status = userService.getActiveStatus(userId);
 		if(status) {
@@ -63,6 +72,7 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/authenticateuser", method=RequestMethod.GET)
+	@ApiOperation(value="Authenticate user", response=ResultBean.class)
 	public ResultBean authenticateUser(String name, String password) throws Exception {
 		Boolean status = userService.authenticateUser(name, password);
 		if(status) {
