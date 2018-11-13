ac625da63bf7632e6a1d8766a245cb766269629c
mylearning-gourav <mylearning.gourav@gmail.com>
2018-11-12 22:57:20 -0500

    Added new sql db file.

---
+++ src/main/resources/new_db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE `library_users`;
+CREATE TABLE `library_users`.`user` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL DEFAULT '',
+  `email` VARCHAR(100) NOT NULL DEFAULT '',
+  `password` VARCHAR(100) NOT NULL DEFAULT '',
+  `active` BOOLEAN NOT NULL DEFAULT 1,
+  `role_id` INTEGER NOT NULL,
+  PRIMARY KEY(`id`)
+)
+ENGINE = InnoDB;
+
+CREATE TABLE `library_users`.`roles` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `role` VARCHAR(45) NOT NULL DEFAULT '',
+  PRIMARY KEY(`id`)
+)
+ENGINE = InnoDB;
