3f7375fc41ce90f7837166e89343bf4173407a9c
mylearning-gourav <mylearning.gourav@gmail.com>
2018-11-19 22:54:59 -0500

    Commented code.

--- src/main/java/com/userservice/ApplicationBoot.java
+++ src/main/java/com/userservice/ApplicationBoot.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 //@EnableEurekaClient
